219c1a89aae8603ab62c70187a5b3d6a98e776da
omar-e <omar.hassanabdi20@gmail.com>
2025-10-20 12:29:04 +0100

    First custom-action push

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('actions/core')
+
+try{
+    const nameToGreet =core.getInput('who-to-greet');
+    console,log(`hello', ${nameToGreet}`);
+    core.setOutput('time', new Date().toTimeString());
+
+} catch (error) {
+    core.setFailed(error.message);
+}
