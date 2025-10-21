0a8b8167ecfed059ed9db0a10a938c9e2881627a
omar-e <omar.hassanabdi20@gmail.com>
2025-10-21 07:46:05 +0100

    console error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('actions/core') // can't find mudule apparently
 
 try{
     const nameToGreet =core.getInput('who-to-greet');
-    console,log(`hello', ${nameToGreet}`);
+    console.log(`hello', ${nameToGreet}`);
     core.setOutput('time', new Date().toTimeString());
 
 } catch (error) {
